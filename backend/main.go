a13584f81a5b2ec5ddbe0eca5db649e696d39da7
ainharan <ainharan.subramaniam@gmail.com>
2020-04-13 20:36:25 +1000

    need to fix bug in getVehicle function - cant figure out string to int comparision

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
@@ -20,20 +19,43 @@ type Vehicle struct {
 var vehicles []Vehicle
 
 func main() {
-	fmt.Println("Hello World!")
 	router := mux.NewRouter()
+	addData()
 	buildRoutes(router)
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-func buildRoutes(router *mux.Router) {
-	router.HandleFunc("/bar", GetInfo).Methods("GET")
+func addData() {
+	vehicles = append(vehicles, Vehicle{ID: 1, Price: 438227.12, Name: "Tesla", numWheels: 4, isManual: false})
+	vehicles = append(vehicles, Vehicle{ID: 2, Price: 54555.13, Name: "Toyota", numWheels: 4, isManual: true})
 }
 
-func GetInfo(writer http.ResponseWriter, request *http.Request) {
-	log.Println("Hi")
-	// initHeaders(writer)
-	json.NewEncoder(writer).Encode(writer)
+func buildRoutes(router *mux.Router) {
+	router.HandleFunc("/vehicles", getVehicles).Methods("GET")
+	router.HandleFunc("/vehicles/{id}", getVehicle).Methods("GET")
+	// router.HandleFunc("/vehicles", createVehicle).Methods("POST")
+	// router.HandleFunc("/vehicles/{id}", updateVehicle).Methods("PUT")
+	// router.HandleFunc("/vehicles/{id}", deleteVehicle).Methods("DELETE")
+}
+
+func getVehicles(writer http.ResponseWriter, request *http.Request) {
+	initHeaders(writer)
+	json.NewEncoder(writer).Encode(vehicles)
+}
+
+func getVehicle(writer http.ResponseWriter, request *http.Request) {
+	initHeaders(writer)
+	params := mux.Vars(request)
+	for _, vehicle := range vehicles {
+		log.Printf("This is the current vehicle's id: %d and here is the param id: %s", vehicle.ID, params["id"])
+		if string(vehicle.ID) == params["id"] {
+			json.NewEncoder(writer).Encode(vehicle)
+			return
+		}
+
+	}
+	log.Printf("Hi this is a meme")
+	json.NewEncoder(writer).Encode(&Vehicle{})
 }
 
 func initHeaders(writer http.ResponseWriter) {
