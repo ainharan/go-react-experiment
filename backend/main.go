5e570e93149f553738b1122926d14faf7916bdb0
ainharan <ainharan.subramaniam@gmail.com>
2020-04-18 20:04:36 +1000

    added updateVehicle method, TODO: handle bad data

--- backend/main.go
+++ backend/main.go
@@ -4,16 +4,18 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
 type Vehicle struct {
-	ID        int     `json:"id"`
-	Name      string  `json:"name,omitempty"`
-	Price     float32 `json:"price,omitempty"`
-	numWheels int     `json:"numWheels,omitempty"`
-	isManual  bool    `json:"isManual"`
+	ID     int     `json:"id"`
+	Name   string  `json:"name,omitempty"`
+	Price  float32 `json:"price,omitempty"`
+	Wheels int     `json:"numWheels,omitempty"`
+	Manual bool    `json:"isManual"`
 }
 
 var vehicles []Vehicle
@@ -26,15 +28,15 @@ func main() {
 }
 
 func addData() {
-	vehicles = append(vehicles, Vehicle{ID: 1, Price: 438227.12, Name: "Tesla", numWheels: 4, isManual: false})
-	vehicles = append(vehicles, Vehicle{ID: 2, Price: 54555.13, Name: "Toyota", numWheels: 4, isManual: true})
+	vehicles = append(vehicles, Vehicle{ID: 1, Price: 438227.12, Name: "Tesla", Wheels: 4, Manual: false})
+	vehicles = append(vehicles, Vehicle{ID: 2, Price: 54555.13, Name: "Toyota", Wheels: 4, Manual: true})
 }
 
 func buildRoutes(router *mux.Router) {
 	router.HandleFunc("/vehicles", getVehicles).Methods("GET")
 	router.HandleFunc("/vehicles/{id}", getVehicle).Methods("GET")
-	// router.HandleFunc("/vehicles", createVehicle).Methods("POST")
-	// router.HandleFunc("/vehicles/{id}", updateVehicle).Methods("PUT")
+	router.HandleFunc("/vehicles", createVehicle).Methods("POST")
+	router.HandleFunc("/vehicles/{id}", updateVehicle).Methods("PUT")
 	// router.HandleFunc("/vehicles/{id}", deleteVehicle).Methods("DELETE")
 }
 
@@ -47,15 +49,34 @@ func getVehicle(writer http.ResponseWriter, request *http.Request) {
 	initHeaders(writer)
 	params := mux.Vars(request)
 	for _, vehicle := range vehicles {
-		log.Printf("This is the current vehicle's id: %d and here is the param id: %s", vehicle.ID, params["id"])
-		if string(vehicle.ID) == params["id"] {
+		if params["id"] == strconv.Itoa(vehicle.ID) {
 			json.NewEncoder(writer).Encode(vehicle)
 			return
 		}
 
 	}
-	log.Printf("Hi this is a meme")
 	json.NewEncoder(writer).Encode(&Vehicle{})
+}
+
+func createVehicle(writer http.ResponseWriter, request *http.Request) {
+	initHeaders(writer)
+	var v Vehicle
+	json.NewDecoder(request.Body).Decode(&v)
+	v.ID = len(vehicles) + 1
+	vehicles = append(vehicles, v)
+	json.NewEncoder(writer).Encode(v)
+}
+
+func updateVehicle(writer http.ResponseWriter, request *http.Request) {
+	initHeaders(writer)
+	var v Vehicle
+	json.NewDecoder(request.Body).Decode(&v)
+	id, err := strconv.Atoi(strings.TrimPrefix(request.RequestURI, "/vehicles/"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	vehicles[id-1] = v
+	json.NewEncoder(writer).Encode(v)
 }
 
 func initHeaders(writer http.ResponseWriter) {
