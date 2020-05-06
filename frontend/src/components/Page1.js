c1c67692a7fa38ac130f7544eff8f05aa8508c0f
ainharan <ainharan.subramaniam@gmail.com>
2020-05-07 09:06:21 +1000

    added a front end to getVehicles

--- frontend/src/components/Page1.js
+++ frontend/src/components/Page1.js
@@ -1,26 +1,7 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 
-import axios from 'axios';
+// import axios from 'axios';
 
-export default class Page1 extends Component {
-  state = {
-    persons: []
-  }
+// export default class Page1 extends Component {
 
-  componentDidMount() {
-    axios.get(`/vehicles`)
-      .then(res => {
-        const persons = res.data;
-        this.setState({ persons });
-        console.log(persons)
-      })
-  }
-
-  render() {
-    return (
-      <ul>
-        { this.state.persons.map(person => <li>{person.name}</li>)}
-      </ul>
-    )
-  }
-}
+// }
--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import { Button, Form } from 'react-bootstrap';
 import axios from 'axios';
 
 
@@ -20,7 +22,24 @@ class App extends Component {
   render() {
     return (
       <div>
-          <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-title">Welcome to React</h1>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Manual" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        {this.state.persons.map(person => <li key={person.id}>{person.name}</li>)}
       </div>
     );
   }
